948f6229a15cc6513af1c317a2e6efc85c0191b1
Jacek Podkanski <ruby.object@googlemail.com>
2012-06-05 14:26:22 +0100

    added inputting of a string

--- script.lisp
+++ script.lisp
@@ -1,6 +1,26 @@
 #!/usr/bin/sbcl --script
 
-(format t "running Lisp~% command line arguments are:~%")
-(format t "~S~%" sb-ext:*posix-argv*)
 
-(format T "~%SBCL Lisp says:~%Hello World!~%")
+(defun run (singular-form)
+  (let ((plural-form (concatenate 'string singular-form "s")) (entered-plural))
+    (format t "singular-form: ~S~%" singular-form )
+    (format t "plural: ~S~%" plural-form)
+    (format t "enter preferred plural form or if above plural is correct just press enter: ~%")
+    (setq entered-plural (read-line ))
+    (format t "you have entered: ~S~%" entered-plural)
+    ))
+
+
+
+(defun usage ()
+  (format t "Enter Singular form eg. Post~%"))
+
+(defun main (args)
+  (let ((options (cdr args)))
+    (progn
+      (if (string= (car options) "--help")
+	  (usage)
+	  (run (car options))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+(main sb-ext:*posix-argv*)
