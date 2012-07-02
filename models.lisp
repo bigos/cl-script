a8aa413eab2b03c7eb0e9135d5afbe47ae5ae453
Jacek Podkanski <ruby.object@googlemail.com>
2012-07-02 23:55:29 +0100

    got filtering working

--- models.lisp
+++ models.lisp
@@ -4,12 +4,24 @@
 (defvar *results* '())
 (defvar *filenames* 
   (directory  "/home/jacek/Programming/work/vps/app/models/*.rb"))
+
+(defun process-line (file line)
+  ;(format T "~A~%" line)
+  (if (search "ActiveRecord" line) (format T "+++++++++++++++++ ~A~%" line))
+  (if (or (search "belongs_to :" line)
+	  (search "has_one :" line)
+	  (search "has_many :" line)
+	  )
+      (format T "*************** ~A~%" line))
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (loop for f in *filenames* do 
      (format T "~%~%PROCESSING... ~A~%~%" f)
 (with-open-file (stream f )
   (do ((line (read-line stream nil)
 	     (read-line stream nil)))
       ((null line))
-    (format T "~A~%" line)))
-
-)
+    (process-line f line)
+    )))
